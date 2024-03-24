4a10e21e28bdd7f37576d2e085e474d2848d05bf
Georgy Dyukov <georgy.dyukov@gmail.com>
2024-03-23 19:40:16 -0700

    Add json formatter

--- gendiff_package/formatters/formatter_choice.py
+++ gendiff_package/formatters/formatter_choice.py
@@ -1,5 +1,6 @@
 from gendiff_package.formatters.stylish import format_stylish
 from gendiff_package.formatters.plain import format_plain
+from gendiff_package.formatters.json import format_json
 
 
 def choose_formatter(data, formatter='stylish'):
@@ -7,5 +8,7 @@ def choose_formatter(data, formatter='stylish'):
         return format_stylish(data)
     elif formatter == 'plain':
         return format_plain(data)
+    elif formatter == 'json':
+        return format_json(data)
     else:
         raise ValueError('Unsupported formatter.')
