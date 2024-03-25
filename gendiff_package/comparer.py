d134b95f96a7a807ef3d6596aa9ca43dd5405b5d
Georgy Dyukov <georgy.dyukov@gmail.com>
2024-03-24 19:04:46 -0700

    Fix comparer.py, generate_diff.py

--- gendiff_package/comparer.py
+++ gendiff_package/comparer.py
@@ -1,7 +1,17 @@
-def compare_dicts(dict1, dict2):
-    key_set_1 = set(dict1)
-    key_set_2 = set(dict2)
-    key_set = key_set_1 | key_set_2
+def compare_data(dict1, dict2):
+    """
+    The function compares two dictionaries and generates
+    a hierarchical difference representation.
+
+    Args:
+        dict1 (dict): The first dictionary for comparison.
+        dict2 (dict): The second dictionary for comparison.
+
+    Returns:
+        dict: A hierarchical representation of the differences
+        between the two dictionaries.
+    """
+    key_set = set(dict1) | set(dict2)
     res = {}
     for key in sorted(key_set):
         if key not in dict1:
@@ -22,7 +32,7 @@ def compare_dicts(dict1, dict2):
         elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
             res[key] = {
                 'status': 'changed, nested',
-                'value': compare_dicts(dict1[key], dict2[key])
+                'value': compare_data(dict1[key], dict2[key])
             }
         else:
             res[key] = {
--- gendiff_package/generate_diff.py
+++ gendiff_package/generate_diff.py
@@ -1,11 +1,25 @@
 from gendiff_package.parser import parser_file
-from gendiff_package.comparer import compare_dicts
+from gendiff_package.comparer import compare_data
 from gendiff_package.formatters.formatter_choice import choose_formatter
 
 
 def generate_diff(file_path_1, file_path_2, formatter='stylish'):
+    """
+    The function reads the content of two configuration files,
+    compares their content, and generates a difference representation.
+    The comparison result is then formatted using the specified formatter.
+
+    Args:
+        file_path_1 (str): The path to the first configuration file.
+        file_path_2 (str): The path to the second configuration file.
+        formatter (str): Optional. Specifies the format of the output.
+            Available options: 'stylish' (default), 'plain', 'json'.
+
+    Returns:
+        str: The generated difference between two configuration files.
+    """
     data1 = parser_file(file_path_1)
     data2 = parser_file(file_path_2)
-    comparison = compare_dicts(data1, data2)
+    comparison = compare_data(data1, data2)
     result = choose_formatter(comparison, formatter)
     return result
